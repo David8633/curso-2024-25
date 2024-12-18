5ce393d976cf23bfeabbf9db9ccda9f5dd5c4137
David8633 <davidinstituto282@gmail.com>
2024-12-18 13:19:44 +0100

    cadenas java

---
+++ PROGRAMCION/java/B1/Cadenas.java
@@ -0,0 +1,13 @@
+package factorial;
+
+public class Cadenas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String cadena = "Esto es una prueba";
+		int posicionLetraU = cadena.indexOf('u');/*muestra la posicion de la letra u*/
+		int ultimaPosicionU = cadena.lastIndexOf('u');/*muestra la posicion de la ultima letra u*/
+		int posicionDeUna = cadena.indexOf("una");/*muestra la posicion de la palabra una*/
+	}
+
+}
