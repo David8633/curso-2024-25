72021eff311be25ef8b81640f351aec32161bb0d
David8633 <davidinstituto282@gmail.com>
2024-12-18 13:52:33 +0100

    teoria de formato de cadenas

---
+++ PROGRAMCION/java/B1/Teoria.java
@@ -0,0 +1,15 @@
+package formato;
+
+
+public class Teoria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = " barco";
+	    String MAY = name.toUpperCase();/*concierte todo en mayuscula*/
+	    String min = name.toLowerCase();/*convierte todo en minuscula*/
+	    String trim = name.trim();/*No borra espacios intermedios, borra los demás espacios*/
+	    System.out.println();
+	}
+
+}
