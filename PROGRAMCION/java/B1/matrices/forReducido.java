5ef8563003d45b430a9f6636fe9cac877cbff864
David8633 <davidinstituto282@gmail.com>
2025-01-13 13:34:35 +0100

    imprime numero por numero

---
+++ PROGRAMCION/java/B1/matrices/forReducido.java
@@ -0,0 +1,14 @@
+package matrices;
+
+public class forReducido {
+
+	public static void main(String[] args) {
+		int [][] matriz = new int [2][2];
+		for (int [] fila:matriz) {
+			for(int celda:fila) {
+				System.out.println(celda);
+			}
+		}
+		}
+	}
+/*imprime paso a paso porque la primera va en horizontal y la segunda en vertical*/
