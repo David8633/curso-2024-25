b43d8e0fcdc95c576498e85873981cca5ae64749
David8633 <davidinstituto282@gmail.com>
2025-03-28 10:04:27 +0100

    JavaScript

---
+++ JavaScript/introducion JavaScript/activadIniciacion.js
@@ -0,0 +1,70 @@
+/*Una variable numero cuyo valor sea -4.*/
+let numero = -4;
+/*Una variable palabra cuyo valor sea ”Hola Mundo”*/
+let palabra = "Hola Mundo";
+/*Una variable booleana cuyo nombre sea respuesta e inicialmente con valor true*/
+let respuesta = true; 
+/*Crea una constante con nombre pi cuyo valor sea 3.14*/
+const pi = 3.14;
+/*Modifica el valor de la variable numero para que en lugar de -4 sea 54 pero 
+sin tocar la parte de la declaración de la variable.*/
+numero = 54;
+/*¿Qué pasa si modificas el valor de la variable antes de que esta sea declarada?*/
+
+/*Modifica el valor de la variable numero para que en lugar de -4 sea 54 pero sin tocar la parte de la declaración de la variable.*/
+
+
+/*Declara 2 variables y asigna los valores 5 y 2 respectivamente*/
+let variabl1 = 5;
+let variable2 = 2;
+/*Muestra por consola la suma de ambas variables.*/
+let numero1 = 1;
+let numero2 = 3;
+
+function sumare(numero1,numero2){
+    return numero1+numero2
+}
+
+const sumar = (numero1,numero2) => {numero1 + numero2}; 
+
+let fin = "Ya se ha realizado la suma"
+let finerroneo = " no se ha realizado la suma"
+(sumar()>7)?console.log(sumar(3,4)):console.log(finerroneo);
+
+/*Muestra por consola la resta de ambas variables.*/
+let n1 = 1;
+let n2 = 3;
+
+function restar(numero1,numero2){
+    return numero1+numero2
+}
+
+const restar = (numero1,numero2) => {numero1 + numero2}; 
+
+let final = "Ya se ha realizado la resta"
+let finerroneos = " no se ha realizado la resta"
+(restar()>7)?console.log(restar(3,4)):console.log(finerroneo);
+
+
+/*Muestra por consola la multiplicación de ambas variables.*/
+let num1 = 1;
+let num2 = 2;
+
+function multiplicación(num1,num2){
+    return num1*num2;
+}
+
+const multiplicación = (numero1,numero2) => (numero1*numero2);
+/*Muestra por consola la división de ambas variables.*/
+function división(num1,num2){
+    return num1/num2;
+}
+
+const división = (num1,num2) => (num1/num2);
+/*Muestra por consola la módulo de ambas variables.*/
+
+console.log(num1%num2)
+/*Declara una variable cuyo valor sea tu nombre.*/
+let nombre = "David";
+/*Muestra por consola el JAVASCRIPT texto Hola concatenado con el valor de la variable anterior.*/
+
