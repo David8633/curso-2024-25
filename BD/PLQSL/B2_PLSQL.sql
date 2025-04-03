df1193e463501bcdaa0d7fd4ecabc2df01a525f6
David8633 <davidinstituto282@gmail.com>
2025-04-03 14:21:08 +0200

    Update B2_PLSQL.sql

--- BD/PLQSL/B2_PLSQL.sql
+++ BD/PLQSL/B2_PLSQL.sql
@@ -1,3 +1,4 @@
+
 /*Escribe un procedimiento que muestre el número de empleados y el salario
 mínimo, máximo y medio del departamento de FINANZAS. Debe hacerse
 uso de cursores implícitos, es decir utilizar SELECT ... INTO.*/
@@ -98,63 +99,73 @@ departamento, hijos). Deber. crearse un cursor explícito al que se le pasarán
 estos parámetros y que mostrar. los datos de los empleados que pertenezcan
 al departamento y con el número de hijos indicados. Al final se indicar. el
 -número de empleados obtenidos.*/
-CREATE OR REPLACE 
-PROCEDURE calcular_cantidad(NUMDEPT NUMBER,NUMHIJOS NUMBER)
-IS
-	v_cantidad number := 0;
 
-	CURSOR C_EMPLEADOS IS
-	SELECT e.numem,e.noemp FROM EMPLEADOS e
-	WHERE e.numde = numdept AND e.numhij = numhijos;
-BEGIN
+CREATE OR REPLACE PROCEDURE ACTIVIDAD1_PLSQL.padre_hijo(v_dep NUMBER , v_hijos NUMBER) 
+IS 
+	CURSOR c_emp IS
+	 SELECT  e.NUMHI, e.NUMDE
+	 FROM EMPLEADOS e 
+	 WHERE v_dep = e.NUMDE
+	 AND v_hijos = e.NUMHI;
+	v_cant NUMBER:=0;
+BEGIN 
+	FOR emp IN c_emp LOOP 
+		DBMS_OUTPUT.PUT_LINE(emp.numde);
+		DBMS_OUTPUT.PUT_LINE(emp.numhi);
+		v_cant:= v_cant + 1;
+	END LOOP;
+	DBMS_OUTPUT.PUT_LINE(v_cant);
+END padre_hijo;
+	
+	
 
-	FOR emp IN c_empleados
-	loop
-		v_cantidad := v_cantidad +1;
-	end loop
-	
-	dbms_output.put_line('cantidad de empleados con dichas caracteristicas : ' || v_cantidad )
-	
+
+BEGIN 
+	padre_hijo(121,2);
 END;
 
-
-CREATE OR REPLACE 
-PROCEDURE(NUMDEPT NUMBER,NUMHIJOS NUMBER)
+CREATE OR REPLACE PROCEDURE ACTIVIDAD1_PLSQL.calcular_cantidad_dept(NUMDEPT NUMBER,NUMHIJOS NUMBER)
 IS
 	v_cantidad number;
 BEGIN
-	CURSOR C_EMPLEADOS IS
 	SELECT count(e.numem)
 	INTO v_cantidad
 	FROM EMPLEADOS e
-	WHERE e.numde = numdept AND e.numhij = numhijos;
-	dbms_output.put_line('cantidad de empleados con dichas caracteristicas : ' || v_cantidad )
-END
+	WHERE e.numde = numdept AND e.numhi = numhijos;
+	dbms_output.put_line('cantidad de empleados con dichas caracteristicas : ' || v_cantidad );
+END;
+
+BEGIN 
+	calcular_cantidad(5,2);
+END;
 
 
 /*4. Escribe un procedimiento con un parámetro para el nombre de empleado,
 que nos muestre la edad de dicho empleado en años, meses y días.*/
 
-CREATE OR replace 
-PROCEDURE MOSTRAR_EDAD(NOEMP VARCHAR2)
-IS
-		
+CREATE OR REPLACE PROCEDURE ACTIVIDAD1_PLSQL.MOSTRAR_EDAD(NOEMP VARCHAR2)
+AS
 	TYPE V_EMPLEADOS IS RECORD(
-		V_AÑOS NUMBER;
-		V_MESE NUMBER;
-		V_DIAS NUMBER;
-
+		V_ANOS NUMBER,
+		V_MESE NUMBER,
+		V_DIAS NUMBER
 	);
-	
+	V_RESUL V_EMPLEADOS;
 BEGIN
-	SELECT E. 
-	INSERT INTO V_EMPLEADOS  
-	FROM EMPLEADOS E;
+	SELECT EXTRACT(YEAR FROM SYSDATE)-EXTRACT(YEAR FROM e.fecna),EXTRACT(month FROM SYSDATE)-EXTRACT(month FROM e.fecna), EXTRACT(day FROM SYSDATE)-EXTRACT(day FROM e.fecna)
+	INTO V_RESUL
+	FROM EMPLEADOS e
+	WHERE E.NOMEM = NOEMP;
+	DBMS_OUTPUT.PUT_LINE('MI AÑO ES: '|| V_RESUL.V_ANOS);
+	DBMS_OUTPUT.PUT_LINE('MI MES ES: '|| V_RESUL.V_MESE);
+	DBMS_OUTPUT.PUT_LINE('MI DIA ES: '|| V_RESUL.V_DIAS); 
+END;
 
-	DBMS_OUTPUT.PTU_LINE('MI EDAD EN AÑOS SERIAN: '|| V_EMPLEADOS.V.AÑOS);
-	DBMS_OUTPUT.PTU_LINE('MI EDAD EN MESES SERIAN: '|| V_EMPLEADOS.V.MESE);
-	DBMS_OUTPUT.PTU_LINE('MI EDAD EN DIAS SERIAN: '|| V_EMPLEADOS.V.DIAS);
-	
-END
+BEGIN
+	MOSTRAR_EDAD('JULIANA');
+END;
+
+SELECT * FROM EMPLEADOS e ;
+
 
 
